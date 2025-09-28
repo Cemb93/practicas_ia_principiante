fa6d34241a8b62ac3239d3d57b549d7c0ee70242
Carlos Martínez <karloz13m@gmail.com>
2025-09-28 11:01:40 -0500

    ejercicio f-string

---
+++ uso_f_string.py
@@ -0,0 +1,8 @@
+name="Carlos"
+last_name="Martinez"
+age=32
+profession="student"
+citizenship="Bogota"
+money="$4.000 USD"
+
+print(f"Nombre {name} \nApellido {last_name} \nEdad: {age} \nProfesion: {profession} \nSoy de {citizenship} y gano {money}")
