e623558fd4f71808eabf13bb8ff3c8b65d3d71c6
Cemb93 <karloz13m@gmail.com>
2025-10-15 15:28:59 -0500

    Subiendo la practica 4 al drive

--- practica_4.py
+++ practica_4.py
@@ -8,40 +8,39 @@ df = pd.read_csv(f'{ruta_archivo}\{nombre_del_archivo}', index_col=0)
 
 df['Aprobado'] = df["Promedio_10"] >= 8.0
 df['Promedio_100'] = df['Promedio_10'] * 10
-# print('========== PRIMER PUNTO ==========')
-# print(f'1 - MANIPULACION DE DATOS:\n{df}\n\n')
+print('========== PRIMER PUNTO ==========')
+print(f'1 - MANIPULACION DE DATOS:\n{df}\n\n')
 
 # TODO: 2. Calcular la media, mediana y la moda de la columna 'Promedio 10'
-# print('========== SEGUNDO PUNTO ==========')
-# print(
-#   f'CALCULANDO LA MEDIA DE (PROMEDIO_10): {df["Promedio_10"].mean()}\n\n'
-#   f'CALCULANDO LA MEDIANA DE (PROMEDIO_10): {df["Promedio_10"].median()}\n\n'
-#   f'CALCULANDO LA MODA DE (PROMEDIO_10): \n{df["Promedio_10"].mode().to_string(index=False)}\n\n'
-# )
+print('========== SEGUNDO PUNTO ==========')
+print(
+  f'CALCULANDO LA MEDIA DE (PROMEDIO_10): {df["Promedio_10"].mean()}\n\n'
+  f'CALCULANDO LA MEDIANA DE (PROMEDIO_10): {df["Promedio_10"].median()}\n\n'
+  f'CALCULANDO LA MODA DE (PROMEDIO_10): \n{df["Promedio_10"].mode().to_string(index=False)}\n\n'
+)
 
 # TODO: 3. Filtrar los estudiantes con 'Aprobado' == True y obtener la lista de nombres
 aprobados = df[df["Aprobado"] == True]
 estudiantes_aprobados = aprobados["Nombre"].tolist()
-# print('========== TERCER PUNTO ==========')
-# print(f'LOS ESTUDIANDO APROBADOS SON:\n{estudiantes_aprobados}\n\n')
+print('========== TERCER PUNTO ==========')
+print(f'LOS ESTUDIANDO APROBADOS SON:\n{estudiantes_aprobados}\n\n')
 
 # TODO: 4. Encontrar la carrera con mayor promedio medio
 # * SE AGRUPA SOLO LA COLUMNA "Carrera"
 # ? SE GENERA UNA COLUMNA LLAMADA "top_carrera" LA CUAL TIENE EL PROMEDIO
 agr = df.groupby("Carrera").agg(top_carrera=("Promedio_10", "max"))
 carrera_mayor_promedio = agr.sort_values("top_carrera", ascending=False).head(1)
-# print('========== CUARTO PUNTO ==========')
-# print(f'CARRERA CON MAYOR PROMEDIO: \n{carrera_mayor_promedio}\n\n')
+print('========== CUARTO PUNTO ==========')
+print(f'CARRERA CON MAYOR PROMEDIO: \n{carrera_mayor_promedio}\n\n')
 
 # TODO: 5. Crear una columna nueva 'Diferencia' = Promedio_100 - (Edad * 2) y explicar el resultado. 
-# print('========== QUINTO PUNTO ==========')
+print('========== QUINTO PUNTO ==========')
 df["Diferencia"] = df["Promedio_100"] - df["Edad"] * 2
-# print(f'CREANDO COLUMNA DIFERENCIA: \n{df}\n\n')
+print(f'CREANDO COLUMNA DIFERENCIA: \n{df}\n\n')
 
 # TODO: 6. Guardar un subconjunto (Nombre, Carrera, Promedio_10, Aprobado) en un nuevo archivo CSV. 
 subset = df[["Nombre", "Carrera", "Promedio_10", "Aprobado"]]
-# print('========== SEXTO PUNTO ==========')
-# print(subset)
+print('========== SEXTO PUNTO ==========')
 
 ruta_archivo_3 = f'{ruta_archivo}\practica4_estudiantes_original.csv'
 subset.to_csv(ruta_archivo_3)
