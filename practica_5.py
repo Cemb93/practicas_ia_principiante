71f1e1431c13d78e519b503eae41ef0c973b92a0
Cemb93 <karloz13m@gmail.com>
2025-10-21 20:27:34 -0500

    Iniciando practica 1

---
+++ practica_5.py
@@ -0,0 +1,38 @@
+# 1. Cargar el fichero de excel 'dataset_bigdata' ty nombrarlo 'df_practica5'
+# 2. Analizar solo eventos batch de redes con tamano_gb >=6
+# 3. Imputar los NaN con la mediana de las columnas 'latencia_ms' y 'etiqueta_anomalia' (separados)
+# 4. Con los nuevos valores de las columnas del punto 3 agrupar por tipo de procesamiento y calcular:
+    # a. Cantidad de registros
+    # b. Media de 'tamano_gb'
+    # c. total de eventos por seg
+
+import pandas as pd
+# R/1.
+ruta_bd = 'C:\Users\USUARIO\Desktop\PROGRAMACION\Artificial Inteligent\Nivel Basico\Base de datos/dataset_bigdata.xlsx'
+df_practica5 = pd.read_excel(ruta_bd)
+print(f'******Punto 1******\n{df_practica5}')
+
+#R/2.
+condicion = (
+    (df_practica5['tipo_procesamiento'] == 'batch') &
+    (df_practica5['origen_datos'] == 'redes') &
+    (df_practica5['tamano_gb'] >= 6)
+)
+df_batch = df_practica5[condicion]
+# print(f'******Punto 2******\n{df_batch}')
+
+#R/3.
+mediana_lms=df_practica5['latencia_ms'].median(skipna=True)
+mediana_etan=df_practica5['etiqueta_anomalia'].median(skipna=True)
+df_practica5_imp=df_practica5.copy()
+df_practica5_imp['latencia_ms']=df_practica5_imp['latencia_ms'].fillna(mediana_lms)
+df_practica5_imp['etiqueta_anomalia']=df_practica5_imp['etiqueta_anomalia'].fillna(mediana_etan)
+# print(f'******Punto 3******\n{df_practica5_imp}')
+
+#R/4.a.
+df_agrupado=df_practica5_imp.groupby('tipo_procesamiento').agg(
+    registros=('id_registro','count'),
+    promedio_tgb=('tamano_gb','mean'),
+    total_even=('tasa_eventos_por_seg','sum')
+).reset_index()
+# print(f'******Punto 4******\n{df_agrupado}')
